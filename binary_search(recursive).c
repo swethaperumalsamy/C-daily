85ffc86939b6b269beabeee7ea44b3322ff6b6f5
Swetha Perumalsamy <144234049+swethaperumalsamy@users.noreply.github.com>
2026-03-20 09:46:44 +0530

    Create binary_search(recursive).c

---
+++ binary_search(recursive).c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, key;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter sorted elements:\n");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    int low = 0, high = n - 1, mid;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key)
+        {
+            printf("Element found at index %d\n", mid);
+            return 0;
+        }
+        else if(arr[mid] < key)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    printf("Element not found\n");
+
+    return 0;
+}
